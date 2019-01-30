e2145637be15dc91235d89ac7f02fb48347e2346
aindriu80 <aindriu80@gmail.com>
2019-01-30 18:49:37 +0000

    Local storage complete

--- resources/appes6.js
+++ resources/appes6.js
@@ -64,16 +64,38 @@ class Store {
     }
     return books;
   }
-  static displayBooks() {}
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI();
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
   static addBook(book) {
     const books = Store.getBooks();
 
-    books.push(books);
+    books.push(book);
 
     localStorage.setItem("books", JSON.stringify(books));
   }
-  static removeBook() {}
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 }
+
+// DOM Load Event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
 
 // Event Listener for book
 document.getElementById("book-form").addEventListener("submit", function(e) {
@@ -113,8 +135,12 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
 document.getElementById(`book-list`).addEventListener(`click`, function(e) {
   // Instantiate UI
   const ui = new UI();
+
   // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show message
   ui.showAlert("Book Removed!", "success");
